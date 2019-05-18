9b9bec66bbc08bb8b8c03d0a67d4c5bc6f368d88
hugrusi <ladasoft.grueso@gmail.com>
2019-05-17 19:35:25 -0500

    Proyecto nuevo
    Se crea nuevo proyecto por error en compilacion de vs 2019

---
+++ ProjectCore/Controllers/ProjectsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using DAL = ProjectCore.DAL;
+
+namespace ProjectCore.Controllers
+{
+    [Authorize]
+    public class ProjectsController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        //private List<Logica.Models.ViewModel.ProjectsIndexViewModel> listProjects;
+
+        public ProjectsController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+
+            //listProjects = new List<Logica.Models.ViewModel.ProjectsIndexViewModel>();
+            //listProjects.Add(new Logica.Models.ViewModel.ProjectsIndexViewModel
+            //{
+            //    Id=1,
+            //    Details = "Project managment details",
+            //    Title = "Project managment",
+            //    ExpectedCompletionDate = DateTime.Now.AddDays(20),
+            //    CreatedAt = DateTime.Now.AddDays(5)
+            //});
+
+            
+            //listProjects.Add(new Logica.Models.ViewModel.ProjectsIndexViewModel
+            //{
+            //    Id = 2,
+            //    Details = "Sales managment details",
+            //    Title = "Sales managment",
+            //    ExpectedCompletionDate = DateTime.Now.AddDays(30),
+            //    CreatedAt = DateTime.Now.AddDays(10)
+            //});
+     
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Logica.Models.BindingModel.ProjectsCreateBindingModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByIdAsync(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+                Logica.BL.Tenants tenants = new Logica.BL.Tenants();
+                var tenant = tenants.GetTenants(user.Id).FirstOrDefault();
+
+                Logica.BL.Projects projects = new Logica.BL.Projects();
+                projects.CreateProjects(model.Title,
+                    model.Details,
+                    model.ExpectedCompletionDate,
+                    tenant.Id);
+
+                return RedirectToAction("Index");
+            }
+
+            //return View();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+
+            IdentityUser user = await _userManager.FindByIdAsync(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            Logica.BL.Tenants tenants = new Logica.BL.Tenants();
+            var tenant = tenants.GetTenants(user.Id).FirstOrDefault();
+
+
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+
+
+            var result = await _userManager.IsInRoleAsync(user, "Admin") ? projects.GetPojects(null, tenant.Id) : projects.GetPojects(null, tenant.Id, user.Id);
+
+            var listProjects = result.Select(x => new Logica.Models.ViewModel.ProjectsIndexViewModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Details = x.Details,
+                CreatedAt = x.CreateAt,
+                ExpectedCompletionDate = x.ExpectedCompletationDate,
+                UpdateAt = x.UpdateAt
+
+            });
+
+            listProjects = tenant.Plan.Equals("premium") ?
+                            listProjects :
+                            listProjects.Take(1).ToList();
+
+            return View(listProjects);
+
+
+        }
+ 
+        public IActionResult Edit(int? id)
+        {
+
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+            var project = projects.GetPojects(id, null).FirstOrDefault();
+
+            var projectBindingModel = new Logica.Models.BindingModel.ProjectsEditBindingModel()
+            {
+                Id = project.Id,
+                Details = project.Details,
+                Title = project.Title,
+                ExpectedCompletionDate = project.ExpectedCompletationDate
+
+            };
+
+            return View(projectBindingModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Logica.Models.BindingModel.ProjectsEditBindingModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Logica.BL.Projects projects = new Logica.BL.Projects();
+                projects.UpdateProjects(model.Id,
+                                        model.Title,
+                                        model.Details,
+                                        model.ExpectedCompletionDate);
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
+        }
+
+        public IActionResult Details(int? id)
+        {
+
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+            var project = projects.GetPojects(id, null).FirstOrDefault();
+
+            var projectViewModel = new Logica.Models.ViewModel.ProjectsDetailsViewModel()
+            {
+                Id = project.Id,
+                Details = project.Details,
+                Title = project.Title,
+                ExpectedCompletionDate = project.ExpectedCompletationDate,
+                CreatedAt = project.CreateAt,
+                UpdateAt = project.UpdateAt
+
+            };
+
+            return View(projectViewModel);
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+
+            projects.DeleteProjects(id);
+
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ ProjectCore/Controllers/TasksController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectCore.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index(int? projectId)
+        {
+            Logica.BL.Tasks tasks = new Logica.BL.Tasks();
+            var task = tasks.GetTasks(projectId, null);
+
+            var taskViewModel = task.Select(x => new Logica.Models.ViewModel.TasksIndexViewModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Details = x.Details,
+                ExpirationDate = x.ExpirationDate,
+                IsCompleted = x.IsCompleted,
+                Effort = x.Effort,
+                RemainingWork = x.RemainingWork,
+                StateId = x.StateId,
+                ActivityId = x.ActivityId,
+                PriorityId = x.PriorityId,
+                ProjectId = x.ProjectId
+            }).ToList();
+
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+            var project = projects.GetPojects(projectId, null).FirstOrDefault();
+            ViewBag.Project = project;
+
+            return View(taskViewModel);
+        }
+
+        public IActionResult Create(int? projectId)
+        {
+            var taskBindingModel = new Logica.Models.BindingModel.TaskCreateBindingModel
+            {
+                ProjectId = projectId
+            };
+
+            Logica.BL.States states = new Logica.BL.States();
+            ViewBag.States = states.GetStates();
+
+            Logica.BL.Activities activities = new Logica.BL.Activities();
+            ViewBag.Activities = activities.GetActivities();
+
+            Logica.BL.Priorities priorities = new Logica.BL.Priorities();
+            ViewBag.Priorities = priorities.GetPriorities();
+
+            return View();
+        }
+
+        public IActionResult Calendar(int? projectId)
+        {
+            Logica.BL.Projects projects = new Logica.BL.Projects();
+            var project = projects.GetPojects(projectId, null).FirstOrDefault();
+
+            ViewBag.Project = project;
+            return View();
+        }
+
+        public IActionResult GetTasksControllers(int? projectId)
+        {
+            Logica.BL.Tasks tasks = new Logica.BL.Tasks();
+            var listTasks = tasks.GetTasks(projectId, null);
+
+            var listTasksCalendarViewModel = listTasks.Select(x => new Logica.Models.ViewModel.TasksGetTasksCalendarViewModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                AllDay = true,
+                Color = "#FFFFFF",
+                Start = x.ExpirationDate.Value.AddDays(Convert.ToDouble(-x.RemainingWork)).ToString("yyyy-MM-dd HH:mm:ss"),
+                End = x.ExpirationDate.Value.ToString("yyyy-MM-dd HH:mm:ss"),
+                TextColor = "#000000"
+            }).ToList();
+
+            return View();
+        }
+
+    }
+}
